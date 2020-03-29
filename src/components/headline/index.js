d6c638cfee95f7db0fcdb64e1d0bf45a8c7847cc
Papagoat <terencelucasyap@gmail.com>
2020-03-30 00:52:26 +0800

    Adding PropTypes to our headline component

--- src/components/headline/index.js
+++ src/components/headline/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Headline extends Component {
 
@@ -20,6 +21,18 @@ class Headline extends Component {
             </div>
         )
     }
+}
+
+Headline.propTypes = {
+    header: PropTypes.string,
+    desc: PropTypes.string,
+    tempArr: PropTypes.arrayOf(PropTypes.shape({
+        fName: PropTypes.string,
+        fNlNameame: PropTypes.string,
+        email: PropTypes.string,
+        age: PropTypes.number,
+        onlineStatus: PropTypes.bool,
+    }))
 }
 
 export default Headline;
